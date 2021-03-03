d6d554e3cdd1e05cda789dde8e92ab38d9558325
Halakhamayseh <khamaysehhala95@yahoo.com>
2021-03-03 16:33:08 +0200

    edit my code

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 'use strict';
-function guess(answerUserInput) {
+let count =0;
+/*function guess(answerUserInput) {
     if (answerUserInput==='yes'|answerUserInput==='y') {
         console.log ('Correct')
         alert ('correct '  +  x)
+        count++;
     }else if (answerUserInput==='no'|answerUserInput==='n') {
         console.log ('wrong')
         alert ('wrong') 
@@ -32,19 +34,19 @@ if (qusetions5==='yes'|qusetions5==='y') {
 }else if (qusetions5==='no'|qusetions5==='n') {
     console.log ('correct')
     alert ('correct '+ ' it is h-a-l-a' ) 
+    count++;
 } else {
     console.log ('you can choose yes,y,no,n')
     alert ('you can choose yes,y,no,n')};
 let userName = prompt('can you tell me your name?');
 alert ('Welcome ' + userName);
-alert ('Thank you' + userName +' for your time');
-let userAge = prompt('can you tell me your age?');
-let qusetions6 = prompt ('sixth qusetion: What is the result of my age + your age?');
-let userAge1 = parseInt(userAge);
-let qusetions61 = parseInt(qusetions6);
-let myAge = 25 ;
-let result= userAge1 + myAge ;
-let opportunities = 4;
+alert ('Thank you' + userName +' for your time');*/
+/*let userAge = prompt('can you tell me your age?');*/
+/*let userAge1 = parseInt(userAge);*/
+/*let qusetions61 = parseInt(qusetions6);*/
+/*let myAge = 25 ;
+let result= userAge1 + myAge ;*/
+let opportunities =0;
 /* this code take a lot of time (8 hours), and it did not run correctly plz give me hints why it did not work
 do {
     switch (result) {  
@@ -63,31 +65,46 @@ do {
     opportunities-=1;
     alert('the true result is' + result);
 } while (opportunities>0);*/
-let myDateAge=5;
-let count =0;
-while (opportunities<=4) { prompt(qusetions6);
-    if (parseInt(qusetions6) === myDateAge) {alert('welldone'); 
+let myBornMonth=5;
+let qusetions6 = prompt ('sixth qusetion: gusess my born moth?');
+while (opportunities<=2) {
+    if (parseInt(qusetions6) === myBornMonth) {alert('welldone'); 
     count++; 
-  
-    }
-    else if (qusetions6 > myDateAge) {alert('too high');
-        
-    }else if (qusetions6 < myDateAge){alert('too low');
-    } else {
-        alert('you can use just number');
-    }
+    console.log(count);
+    break;
+}else if (parseInt (qusetions6) > myBornMonth) {alert('too high');
     opportunities+=1; 
- alert('the true result is' + result) 
+    console.log(opportunities);
+    qusetions6=prompt('sixth qusetion: gusess my born moth?');
+    }else if (parseInt (qusetions6) < myBornMonth){alert('too low');
+    opportunities+=1; 
+    console.log(opportunities);
+    qusetions6=prompt('sixth qusetion: gusess my born moth?');
+    }
+     else {
+        alert('you can use just number');
+        opportunities+=1;
+        qusetions6=prompt('sixth qusetion: gusess my born moth?');
+        console.log(opportunities); 
+    }
 }
+ alert('the true result is' +myBornMonth) ;
+/*q7*/
+opportunities=0
 let qusetions7answer =['dark','pink','yellow']
 let hint=alert('first color as dark , second as a rose and the third as a sun')
-for (let i = 0; i < 6; i++) {
-    let qusetions7 = prompt ('seventh qusetion: What are My favorite colors ?');
-    if (qusetions7='dark'||'pink'||'yellow') {alert('it is correct');
+let qusetions7 = prompt ('seventh qusetion: What are My favorite colors ?');
+while (opportunities<5) {
+for (let i = 0; i <qusetions7answer.length; i++) {
+    if (qusetions7===qusetions7answer[i]) {alert('it is correct');
     count++;
-    } else(qusetions7!='dark'||'pink'||'yellow'){ alert('thank you');
-    alert (qusetions7answer)  
+    opportunities+=10
+    break;
     }  
-} alert(count)
-
+}
+alert('try again'+hint);
+opportunities+=1;
+qusetions7 = prompt ('seventh qusetion: What are My favorite colors ?');
+}
+alert(count /'out/7');
 
